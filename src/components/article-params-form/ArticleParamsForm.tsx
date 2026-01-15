451323110a31980194d1cec44551bf9547f579ca
Mikhail Kartsev <mh.kartsev100404@gmail.com>
2026-01-15 07:29:53 +0300

    первый коммит
    Исправленный коммит

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,20 +1,153 @@
+import { useState, useRef } from 'react';
+import clsx from 'clsx';
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
-
+import { Select } from 'src/ui/select';
+import { RadioGroup } from 'src/ui/radio-group';
+import { Separator } from 'src/ui/separator';
+import { Text } from 'src/ui/text';
+import { useOutsideClickClose } from 'src/ui/select/hooks/useOutsideClickClose';
+import {
+	fontFamilyOptions,
+	fontColors,
+	backgroundColors,
+	contentWidthArr,
+	fontSizeOptions,
+	defaultArticleState,
+} from 'src/constants/articleProps';
+import type { OptionType, ArticleStateType } from 'src/constants/articleProps';
 import styles from './ArticleParamsForm.module.scss';
 
-export const ArticleParamsForm = () => {
+type ArticleParamsFormProps = {
+	onApply: (newState: ArticleStateType) => void;
+};
+
+export const ArticleParamsForm = ({ onApply }: ArticleParamsFormProps) => {
+	const [isOpen, setIsOpen] = useState<boolean>(false);
+	const [formState, setFormState] =
+		useState<ArticleStateType>(defaultArticleState);
+	const rootRef = useRef<HTMLDivElement | null>(null);
+
+	useOutsideClickClose({
+		isOpen,
+		rootRef,
+		onClose: () => setIsOpen(false),
+		onChange: setIsOpen,
+	});
+
+	const toggleOpen = () => {
+		setIsOpen((prevOpenState) => !prevOpenState);
+	};
+
+	const handleFontFamilyChange = (option: OptionType) => {
+		setFormState({ ...formState, fontFamilyOption: option });
+	};
+
+	const handleFontSizeChange = (option: OptionType) => {
+		setFormState({ ...formState, fontSizeOption: option });
+	};
+
+	const handleFontColorChange = (option: OptionType) => {
+		setFormState({ ...formState, fontColor: option });
+	};
+
+	const handleBackgroundColorChange = (option: OptionType) => {
+		setFormState({ ...formState, backgroundColor: option });
+	};
+
+	const handleContentWidthChange = (option: OptionType) => {
+		setFormState({ ...formState, contentWidth: option });
+	};
+
+	const handleReset = () => {
+		setFormState(defaultArticleState);
+		onApply(defaultArticleState);
+		setIsOpen(false);
+	};
+
+	const handleApply = () => {
+		onApply(formState);
+		setIsOpen(false);
+	};
+
+	const handleSubmit: React.FormEventHandler<HTMLFormElement> = (event) => {
+		event.preventDefault();
+		handleApply();
+	};
+
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
-				<form className={styles.form}>
-					<div className={styles.bottomContainer}>
-						<Button title='Сбросить' htmlType='reset' type='clear' />
-						<Button title='Применить' htmlType='submit' type='apply' />
-					</div>
-				</form>
-			</aside>
+			<ArrowButton isOpen={isOpen} onClick={toggleOpen} />
+			{isOpen && (
+				<aside
+					className={clsx(styles.container, styles.container_open)}
+					ref={rootRef}>
+					<form className={styles.form} onSubmit={handleSubmit}>
+						{/* Заголовок */}
+						<Text as='h2' size={31} weight={800}>
+							Задайте параметры
+						</Text>
+
+						{/* Шрифт */}
+						<Select
+							title='Шрифт'
+							selected={formState.fontFamilyOption}
+							options={fontFamilyOptions}
+							onChange={handleFontFamilyChange}
+						/>
+
+						{/* Размер шрифта */}
+						<RadioGroup
+							name='fontSize'
+							title='Размер шрифта'
+							options={fontSizeOptions}
+							selected={formState.fontSizeOption}
+							onChange={handleFontSizeChange}
+						/>
+
+						{/* Цвет шрифта */}
+						<Select
+							title='Цвет шрифта'
+							selected={formState.fontColor}
+							options={fontColors}
+							onChange={handleFontColorChange}
+						/>
+
+						<Separator />
+
+						{/* Цвет фона */}
+						<Select
+							title='Цвет фона'
+							selected={formState.backgroundColor}
+							options={backgroundColors}
+							onChange={handleBackgroundColorChange}
+						/>
+
+						{/* Ширина контента */}
+						<Select
+							title='Ширина контента'
+							selected={formState.contentWidth}
+							options={contentWidthArr}
+							onChange={handleContentWidthChange}
+						/>
+
+						{/* Кнопки */}
+						<div className={styles.bottomContainer}>
+							<Button
+								title='Сбросить'
+								type='clear'
+								htmlType='reset'
+								onClick={handleReset}
+							/>
+							<Button
+								title='Применить'
+								type='apply'
+								htmlType='submit'
+							/>
+						</div>
+					</form>
+				</aside>
+			)}
 		</>
 	);
 };
